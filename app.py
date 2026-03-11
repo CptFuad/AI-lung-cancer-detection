fa2c6e3656e186e060e38b9cfa3d617abe5fdf87
CptFuad <mdfatinfuad56@gmail.com>
2026-03-12 02:33:18 +0600

    delete gadcam

--- app.py
+++ app.py
@@ -13,30 +13,6 @@ class_names=['Normal','Benign', 'Malignant']
 uploaded_file=st.file_uploader('Upload the CT Scan Image', type=['jpg', 'png'])
 
 
-def make_gradcam_heatmap(img_array, model, last_conv_layer_name):
-
-    grad_model = tf.keras.models.Model(
-        [model.inputs],
-        [model.get_layer(last_conv_layer_name).output, model.output]
-    )
-
-    with tf.GradientTape() as tape:
-        conv_outputs, predictions = grad_model(img_array)
-        class_channel = predictions[:, np.argmax(predictions[0])]
-
-    grads = tape.gradient(class_channel, conv_outputs)
-
-    pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
-
-    conv_outputs = conv_outputs[0]
-
-    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
-    heatmap = tf.squeeze(heatmap)
-
-    heatmap = np.maximum(heatmap,0) / np.max(heatmap)
-
-    return heatmap.numpy()
-
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded CT Scan Image')
@@ -54,16 +30,3 @@ if uploaded_file is not None:
 
     for i in range(len(class_names)):
         st.write(class_names[i], ':', round(prob[i]*100,2), '%')
-
-
-# GradCAM
-    heatmap = make_gradcam_heatmap(img, model, "top_conv")
-
-    heatmap = cv2.resize(heatmap, (224,224))
-    heatmap = np.uint8(255 * heatmap)
-
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-
-    superimposed_img = heatmap * 0.4 + np.array(image.resize((224,224)))
-
-    st.image(superimposed_img.astype("uint8"), caption="Heatmap Visualization")
